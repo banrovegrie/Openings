7cf165082892b02778f99e9e1658d45db9c9b4d0
Arjo <ac.ala.arya@gmail.com>
2021-01-11 23:42:57 +0530

    smol changes

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ const mongoose = require('mongoose')
 // To get data fro dotfiles
 require('dotenv').config();
 
+
 // Initializing the app
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 // Connecting to Database and check if connected
 const uri = process.env.ATLAS_URI;
@@ -20,11 +22,16 @@ const options = {
     useUnifiedTopology: true, 
     useCreateIndex: true 
 };
-mongoose.connect(uri, options).then(() => console.log('MongoDB connected')).catch((err) => console.log(err));
+mongoose
+    .connect(uri, options)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
 });
+
 
 // Check if connection works from port
 app.listen(port, () => {
